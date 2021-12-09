eda06f908cd98cc7fd0e20ad685b82bf6b3621b0
Drake Lambert <drake-lambert@outlook.com>
2021-12-08 21:21:18 -0600

    day 4 - parse the input

--- 4/Main.hs
+++ 4/Main.hs
@@ -1,14 +1,65 @@
+import Data.Maybe (fromJust, isNothing)
+import Text.Read (readMaybe)
+
 main = do
   input <- lines <$> readFile "./input.txt"
 
-  let drawnNumbers = split ',' (head input)
+  let drawnNumbers = inputToDrawnNumbers input
 
   print drawnNumbers
 
-split :: Char -> String -> [String]
-split _ "" = [[]]
-split separator (x : xs) =
-  let next = split separator xs
+  let boards = inputToBoards input
+
+  print boards
+
+inputToDrawnNumbers :: [String] -> [Int]
+inputToDrawnNumbers xs =
+  let firstLine = head xs
+      numbers = splitString ',' firstLine
+   in map read numbers
+
+inputToBoards :: [String] -> [Board]
+inputToBoards [] = []
+inputToBoards (x : xs)
+  | ',' `elem` x = next
+  | x == "" = next
+  | otherwise = stringsToBoard currentBoardStrings : inputToBoards dropFive
+  where
+    next = inputToBoards xs
+    currentBoardStrings = x : take 4 xs
+    dropFive = drop 4 xs
+
+newtype Row = Row [Int]
+
+instance Show Row where
+  show (Row numbers) = show numbers
+
+stringToRow :: String -> Row
+stringToRow xs
+  | length numbers /= 5 = error "string must contain exactly 5 numbers"
+  | otherwise = Row numbers
+  where
+    numbers = map read (words xs)
+
+newtype Board = Board [Row]
+
+instance Show Board where
+  show (Board rows) = show rows
+
+stringsToBoard :: [String] -> Board
+stringsToBoard xs
+  | length xs /= 5 = error "list must contain exactly 5 strings"
+  | otherwise = Board (map stringToRow xs)
+
+splitString :: Char -> String -> [String]
+splitString _ "" = [[]]
+splitString separator (x : xs) =
+  let next = splitString separator xs
    in if x == separator
         then "" : next
         else (x : head next) : tail next
+
+joinStrings :: String -> [String] -> String
+joinStrings _ [] = ""
+joinStrings _ [x] = x
+joinStrings separator (x : xs) = x ++ separator ++ joinStrings separator xs
